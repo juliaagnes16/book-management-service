8649d3ea10516b834cbedd5c4c5b18750225390f
Julia Agnes Furqonil Iman <julia.iman@ist.id>
2024-08-05 10:27:33 +0700

    fix(quiz-3): add .env

--- Quiz 3/book-management/main.go
+++ Quiz 3/book-management/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"main.go/database"
 	"main.go/routes"
 )
 
+var (
+	DB  *sql.DB
+	err error
+)
+
 func main() {
+	err = godotenv.Load("config/.env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	DB, err = sql.Open("postgres", psqlInfo)
+	defer DB.Close()
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	database.InitDB()
 	router := routes.InitRoutes()
 	if err := router.Run(":8080"); err != nil {
